458a543754ea49c6523d8adb9af768d76f72ce66
X3TSr <kylian1202@gmail.com>
2025-05-30 16:34:27 +0200

    feat: Add more customisation on button component

--- app/src/Components/Button/Button.jsx
+++ app/src/Components/Button/Button.jsx
@@ -1,13 +1,49 @@
-import { forwardRef } from 'react';
+import { Children, forwardRef } from 'react';
 import style from './Button.module.css'
 
 import * as modules from '../../general-js/scripts'
 
-const Button = forwardRef(({ type = 'primary', text = 'Button', classN, onclick = () => { } }, ref) => {
+const Button = forwardRef(({
+    width = '100%',
+    height = 'unset',
+    type = 'primary',
+    text = 'Button',
+    classN,
+    onclick = () => { },
+    fontSize = 'p',
+}, ref
+) => {
+
+    const checkFontSize = () => {
+        switch (fontSize) {
+            case 'h1':
+                return <h1>{text}</h1>
+            case 'h2':
+                return <h2>{text}</h2>
+            case 'h3':
+                return <h3>{text}</h3>
+            case 'h4':
+                return <h4>{text}</h4>
+            case 'p':
+                return <p>{text}</p>
+
+            default:
+                break;
+        }
+    }
 
     return (
         <>
-            <button className={`${style.button} ${classN} ${style[`btnType${modules.textCasingModule.toSentenceCase(type)}`]}`} onClick={onclick} ref={ref}>{text}</button >
+            <button
+                style={{ width: `${width}`, height: `${height}` }}
+                className={`${style.button} ${classN} ${style[`btnType${modules.textCasingModule.toSentenceCase(type)}`]}`}
+                onClick={onclick}
+                ref={ref}
+            >
+                {
+                    checkFontSize()
+                }
+            </button >
         </>
     )
 })
