f907f5bec2baf7ebf4bd97f94d838553cc4a41c6
X3TSr <kylian1202@gmail.com>
2025-05-30 16:34:50 +0200

    feat: Add more customisation on card component

--- app/src/Components/Card/Card.jsx
+++ app/src/Components/Card/Card.jsx
@@ -3,29 +3,49 @@ import style from './Card.module.css'
 
 import * as modules from '../../general-js/scripts'
 
-const Card = ({ size = [0, 0], theme = 'dark', type = 'default', content = '' }) => {
+const Card = ({
+    size = [0, 0],
+    classN = '',
+    theme = 'dark',
+    type = 'default',
+    number = 'XXX.XX',
+    content = '',
+    subDescription = '',
+    subNumber = '',
+}) => {
 
     const checkType = () => {
         switch (type) {
             case 'budgetText':
                 return (
                     <div className={`${style.alignLeft}`}>
-                        <h2>€ XXX.XX</h2>
-                        <h4>{content}</h4>
+                        <h2>€ {number}</h2>
+                        <h4 className={`${style.cardTitle}`}>{content}</h4>
                     </div>
                 )
             case 'budgetTextG':
                 return (
                     <div className={`${style.alignLeft}`}>
-                        <h2 style={{ color: `var(--color-green)` }}>€ XXX.XX</h2>
-                        <h4>{content}</h4>
+                        <h2 style={{ color: `var(--color-green)` }}>€ {number}</h2>
+                        <h4 className={`${style.cardTitle}`}>{content}</h4>
                     </div>
                 )
             case 'budgetTextR':
                 return (
                     <div className={`${style.alignLeft}`}>
-                        <h2 style={{ color: `var(--color-red)` }}>€ XXX.XX</h2>
-                        <h4>{content}</h4>
+                        <h2 style={{ color: `var(--color-red)` }}>€ {number}</h2>
+                        <h4 className={`${style.cardTitle}`}>{content}</h4>
+                    </div>
+                )
+            case 'percent':
+                return (
+                    <div className={`${style.alignCenter}`}>
+                        <h4 className={`${style.cardTitle}`}>{content}</h4>
+                        <h1 style={{ color: 'var(--color-green)' }}>{number} %</h1>
+                        <div className={`${style.alignCenter} ${style.cardTitle}`}>
+                            <p>{subDescription}</p>
+                            <h4>{subNumber}</h4>
+                        </div>
                     </div>
                 )
 
@@ -38,7 +58,7 @@ const Card = ({ size = [0, 0], theme = 'dark', type = 'default', content = '' })
 
     return (
         <>
-            <div /* style={{ width: size[0], height: size[1] }} */ className={`${style.card}`}>
+            <div /* style={{ width: size[0], height: size[1] }} */ className={`${style.card} ${classN}`}>
                 {checkType()}
                 <div className={`${style.background} ${style[theme]}`}></div >
             </div >
