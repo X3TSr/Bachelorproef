94b3128bc90b9a8b3bec7d31cc650ecc379b3b25
X3TSr <kylian1202@gmail.com>
2025-06-04 14:16:16 +0200

    Update: Finished V1 of the dashboard

--- app/src/Pages/Dashboard/Dashboard.jsx
+++ app/src/Pages/Dashboard/Dashboard.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import { Link } from 'react-router-dom';
 import style from './Dashboard.module.css'
 
+import * as modules from '../../general-js/scripts'
+import ROUTES from '../../consts/ROUTES';
 import useFetchUser from '../../hooks/useFetchUser';
+import useFetchData from '../../hooks/useFetchData';
+
+import testData from '../../consts/testData.json';
 
 import Button from '../../Components/Button/Button';
 import Card from '../../Components/Card/Card';
+import Loading from '../../Components/Loading/Loading';
+import useDataFunctions from '../../hooks/useDataFunctions';
+import Intro from '../Intro/Intro';
 
 const Dashboard = () => {
 
     const { user, loading, error } = useFetchUser();
+    const { data } = useFetchData();
+    const {
+        getYearNet,
+        getAllTimeHighestIncome,
+        getAllTimeNet,
+        getAllTimeTotalIncome,
+        getAllTimeTotalExpenses
+    } = useDataFunctions();
 
-    if (loading) return <p>Loading...</p>
-    if (error) return <p>{error}</p>
+    if (loading || !data) return <Loading />
+    if (error) return <p style={{ color: 'var(--color-red)' }}>Error: {error}</p>
     if (!user) return <p>Nothing to show</p>
 
 
-    return (
+    if (user.firstSignin) return <Intro />
+
+    // console.log(data);
+
+    if (data) return (
         <>
             <section className={`${style.sectionDashboard}`}>
                 <div className={`${style.title}`}>
@@ -24,16 +45,15 @@ const Dashboard = () => {
                 </div>
                 <div className={`${style.content}`}>
                     <div className={`${style.cardContainer}`}>
-                        <div className={`${style.top}`}>
-                            <Card type='budgetText' content='Total' />
-                            <Card type='budgetTextG' content='Income' />
-                            <Card type='budgetTextR' content='Expenses' />
-                        </div>
-                        <div className={`${style.bottom}`}>
-
-                        </div>
+                        <Card type='budgetText' content='Total' number={getAllTimeNet()} />
+                        <Card type='budgetTextG' content='Income' number={getAllTimeTotalIncome()} />
+                        <Card type='budgetTextR' content='Expenses' number={getAllTimeTotalExpenses()} />
+                        <Card type='graph' classN='span2' content='This Year Net' number={getYearNet()} />
+                        <Card type='percent' content='Year High Income' number={(getAllTimeHighestIncome().value / getAllTimeTotalIncome() * 100).toFixed(2)} subDescription={getAllTimeHighestIncome().label} subNumber={getAllTimeHighestIncome().value} />
                     </div>
-                    <Button type='primary' />
+                    <Link to={ROUTES.cashflow}>
+                        <Button height='auto' type='primary' fontSize='h4' text='Add Cashflow' />
+                    </Link>
                 </div>
             </section>
         </>
