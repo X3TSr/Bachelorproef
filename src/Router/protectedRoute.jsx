45ad8f96a446d82be0fb2882ba1d3bb4c322bda6
X3TSr <kylian1202@gmail.com>
2025-06-12 11:00:48 +0200

    Update: Reworked protected route to work using react useNavigate instead of location.href replace

--- src/Router/protectedRoute.jsx
+++ src/Router/protectedRoute.jsx
@@ -1,12 +1,20 @@
+import { useNavigate } from 'react-router-dom'
 import { useUserStore } from '../Store/userStore'
+import { useEffect } from 'react';
+
+import Loading from '../Components/Loading/Loading';
 
 export default function ProtectedRoute({ children }) {
     const isLoggedIn = useUserStore((state) => state.isLoggedIn)
+    const navigate = useNavigate();
 
-    if (!isLoggedIn) {
-        location.href = location.href.replace(/\/.*/gm, '/login')
-    }
-    else {
-        return children
-    }
+    useEffect(() => {
+        if (!isLoggedIn) {
+            navigate('/login', { replace: true })
+        }
+    }, [isLoggedIn, navigate])
+
+    if (!isLoggedIn) return <Loading />
+
+    return children
 }
