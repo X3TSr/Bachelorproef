0c51ec78e180f6d2b6eae8bdc91bd8a88e915b62
X3TSr <kylian1202@gmail.com>
2025-06-13 16:45:00 +0200

    Update: Seperate Transaction into it's own component

--- src/Components/Transaction/Transaction.jsx
+++ src/Components/Transaction/Transaction.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import * as modules from '../../../general-js/scripts'
+import * as modules from '../../general-js/scripts'
 
 const Transaction = ({ transaction }) => {
 
